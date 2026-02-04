ad01abaf9c3b0124f15a12a3b6de249f3ccc5c06
AhmedMohamed <ahmed884822277@gmail.com>
2026-02-04 15:00:46 +0200

    bouns.js

---
+++ bouns.js
@@ -0,0 +1,17 @@
+const longestCommonPrefix = (strs) => {
+  if (strs.length === 0) return "";
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    while (!strs[i].startsWith(prefix)) {
+      prefix = prefix.slice(0, -1);
+      if (prefix === "") return "";
+    }
+  }
+  return prefix;
+};
+console.log(
+  "Example 1: " + `${longestCommonPrefix(["flower", "flow", "flight"])}`,
+);
+console.log(
+  "Example 2: " + `${longestCommonPrefix(["dog", "racecar", "car"])}`,
+);
